107591740e1aebe01cba6801d63b38669558c73f
Dawid Wesolowski <dawaw7@wp.pl>
2021-01-19 12:08:57 +0100

    dodanie obsługi recaptchy do GUS

--- app/Helpers/RecaptchaHelper.php
+++ app/Helpers/RecaptchaHelper.php
@@ -7,16 +7,16 @@ use Illuminate\Support\Str;
 
 class RecaptchaHelper {
     public $url = 'https://www.google.com/recaptcha/api/siteverify?secret=??&response=??';
-    public $secrect;
+    public $secret;
     public $postValue;
 
     public function __construct(?string $string){
-        $this->secrect = env('GOOGLE_RECAPTCHA_SECRET');
+        $this->secret = env('GOOGLE_RECAPTCHA_SECRET');
         $this->postValue = $string;
     }
 
     public function check()  {
-        $checkUrl = Str::replaceArray('??', [$this->secrect, $this->postValue], $this->url);
+        $checkUrl = Str::replaceArray('??', [$this->secret, $this->postValue], $this->url);
         $response = json_decode(file_get_contents($checkUrl));
 
         return $response->success;
--- resources/views/gus.blade.php
+++ resources/views/gus.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app-fronted')
 
 @section('content')
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     <section class="site-section">
         <div class="container">
             <div class="row mb-4">
@@ -12,28 +13,36 @@
                 <div class="col-md-12 col-lg-8 main-content">
                   <p>Aplikacja służy do pobierania danych z GUS. Należy podać numer NIP lub REGON albo KRS (w przypadku osoby prawnej będzie możliwy
                       do pobrania szczegółowy raport).</p>
-                <form action="">
+                    <p class="small">Aplikacja jest ciągle rozwijana. Jeżeli mają Państwo jakieś uwagi lub sugestie, proszę kierować
+                        je na mail-a - bedę bardzo wdzięczny.</p>
+                <form action="{{route('ajax_gus')}}" method="post" id="gus">
                     <div class="row">
-                        <div class="form-group col-5">
+                        <div class="form-group col-md-7 col-xl-5">
                             <label for="nip">Numer NIP</label>
                             <input type="number" name="nip" id="nip" min="0" class="form-control">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-5">
+                        <div class="form-group col-md-7 col-xl-5">
                             <label for="regon">Numer REGON</label>
                             <input type="number" name="regon" id="regon" min="0" class="form-control">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group col-5">
+                        <div class="form-group col-md-7 col-xl-5">
                             <label for="krs">Numer KRS</label>
                             <input type="number" name="krs" id="krs" min="0" class="form-control">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6 form-group">
-                            <input id="check" type="button" value="Szukaj" class="btn btn-primary">
+                        <div class="col-md-7 col-xl-5">
+                            <div  class="g-recaptcha mt-4 mb-4"
+                                  data-sitekey="{{env('GOOGLE_RECAPTCHA_KEY')}}"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-7 form-group">
+                            <input id="check" type="submit" value="Szukaj" class="btn btn-primary">
                         </div>
                     </div>
                 </form>
@@ -45,13 +54,13 @@
                     $(function(){
                         let form = {
                             form: ['nip', 'regon', 'krs'],
-                            submitButton: $('#check'),
+                            formElement: $('#gus'),
                             messageNipKrs: 'Pole może składać się wyłącznie z 10 cyfr',
                             messageRegon: 'Pole może składać się wyłącznie z 9 cyfr',
                             editField: null,
                             onInit: function (){
                                 this.addEvents();
-                                this.submitButton.click((e) => {
+                                this.formElement.submit((e) => {
                                     this.submit(e);
                                 });
                             },
@@ -87,8 +96,9 @@
                                 $('input[type=number]').val(null).attr('disabled', false);
                             },
                             submit: function (e){
-                                var that = this;
                                 e.preventDefault();
+                                var that = this;
+
                                 if(!this.validate()){
                                     this.clearFields();
                                 }else{
@@ -100,18 +110,26 @@
                                     $.ajax({
                                         method: 'post',
                                         url: '{{route('ajax_gus')}}',
-                                        data: {nip: $('#nip').val(), regon: $('#regon').val(), krs: $('#krs').val()},
+                                        data: {
+                                            nip: $('#nip').val(),
+                                            regon: $('#regon').val(),
+                                            krs: $('#krs').val(),
+                                            g_recaptcha_response: grecaptcha.getResponse(),
+                                        },
                                         success: function (data){
-                                            that.clearFields();
-                                            let basic = data.response.basic;
-                                            let report = data.response.report[0];
-                                            let button = '';
-                                            if(typeof report.ErrorCode === 'undefined'){
-                                                button = '<a class="btn btn-outline-primary" target="_blank" href="{{route('ajax_gus_pdf')}}">Pobierz raport</a>'
-                                            }
-                                            // console.log(typeof report.ErrorCode === 'undefined' );
-                                            $('#display')
-                                                .html(`
+                                            if (data.response.error) {
+                                                alert(data.response.error);
+                                            }else {
+                                                that.clearFields();
+                                                let basic = data.response.basic;
+                                                let report = data.response.report[0];
+                                                let button = '';
+                                                if (typeof report.ErrorCode === 'undefined') {
+                                                    button = '<a class="btn btn-outline-primary" target="_blank" href="{{route('ajax_gus_pdf')}}">Pobierz raport</a>'
+                                                }
+                                                // console.log(typeof report.ErrorCode === 'undefined' );
+                                                $('#display')
+                                                    .html(`
                                                     <p class="mt-3"><b>Nazwa:</b> ${basic.name}</p><p><b>Województwo:</b> ${basic.province}</p>
                                                     <p><b>Powiat:</b> ${basic.district}</p><p><b>Gmina:</b> ${basic.community}</p>
                                                     <p><b>Miasto:</b> ${basic.city}</p><p><b>Kod pocztowy:</b> ${basic.zip}</p>
@@ -120,12 +138,14 @@
                                                     <p><b>Nip:</b> ${basic.nip}</p><p><b>Status NIP:</b> ${basic.nipStatus}</p>
                                                     <p><b>Data zakończenia działaności:</b> ${basic.activityEnd}</p>${button}
                                                     `);
+                                            }
                                         },
                                         fail: function (data){
-                                            console.log('failed');
+                                           alert('Błąd z aplikacją. Spróbuj później');
                                         }
                                     })
                                 };
+                                return false;
 
 
                             },
@@ -134,6 +154,10 @@
                                     alert('Uzupełnij pole!')
 
                                     return false;
+                                }
+                                let recaptchaResponse = grecaptcha.getResponse();
+                                if (recaptchaResponse.length === 0) {
+                                    alert('Zaznacz pole z Catpchą');
                                 }
                                 let name = this.editField.attr('name');
                                 let value = this.editField.val().toString();
